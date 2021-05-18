c567ee04f8e13d557053e6b547bb9ee0416b9a33
Derian Castro <deriancastro@hotmail.com>
2021-05-18 20:14:13 +0200

    Add intro Testing and compenent´s test

---
+++ src/Player.spec.js
@@ -0,0 +1,25 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import Player from './Player'
+describe('Player', () => {
+  it('returns a name, score and two buttons', async () => {
+    render(
+      <Player name="John" score={55} onPlus={() => {}} onMinus={() => {}} />
+    )
+    const buttons = screen.getAllByRole('button')
+    expect(screen.getByText('John')).toBeInTheDocument()
+    expect(screen.getByText('55')).toBeInTheDocument()
+    expect(buttons.length).toBe(2)
+  })
+  it('calls onPlus and onMinus correctly', async () => {
+    const OnPlus = jest.fn()
+    const OnMinus = jest.fn()
+    render(<Player name="John" score={0} onPlus={OnPlus} onMinus={OnMinus} />)
+    const plusButton = screen.getByRole('button', { name: '+' })
+    userEvent.click(plusButton)
+    expect(OnPlus).toBeCalled()
+    const minusButton = screen.getByRole('button', { name: '-' })
+    userEvent.click(minusButton)
+    expect(OnMinus).toBeCalled()
+  })
+})
